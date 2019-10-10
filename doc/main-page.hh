f87319af623057971144321d666c3e8a8de1e584
Florent Lamiraux <florent@laas.fr>
2019-10-10 09:19:15 +0200

    [Doc] add some documentation.

--- doc/main-page.hh
+++ doc/main-page.hh
@@ -2,10 +2,33 @@
 
   Communication between HPP and Agimus.
 
-  \par
-  The main classes are:
-  \li agimus_hpp.trajectory_publisher.HppOutputQueue
-  \li agimus_hpp.estimation.Estimation
-  \li agimus_hpp.planning_request_adapter.PlanningRequestAdapter
+  This package implements a ROS node through script hpp_node.py. The script
+  creates one instance of the 3 following classes:
 
+  \li \link agimus_hpp.trajectory_publisher.HppOutputQueue HppOutputQueue
+  \endlink
+  \li \link agimus_hpp.estimation.Estimation  Estimation \endlink
+  \li \link agimus_hpp.planning_request_adapter.PlanningRequestAdapter
+  PlanningRequestAdapter \endlink
+
+  \link agimus_hpp.trajectory_publisher.HppOutputQueue HppOutputQueue
+  \endlink
+  initializes a client to \c hppcorbaserver and samples reference trajectories
+  of various tasks in topics. For instance
+  \li \c /hpp/target/op_fame/talos/arm_left7_joint for the reference position
+      of joint \c arm_left7_joint of Talos robot,
+  \li \c /hpp/target/velocity/op_fame/talos/arm_left7_joint for the reference
+      velocity of joint \c arm_left7_joint,
+  \li \c /hpp/target/position for the reference value of the posture task,
+  \li \c /hpp/target/velocity for the reference velocity of the posture,
+  \li \c /hpp/target/com/talos for the reference trajectory of the center
+      of mass,
+  \li \c /hpp/target/velocity/com/talos for the reference velocity of the center of mass.
+
+  \link agimus_hpp.estimation.Estimation  Estimation \endlink computes an
+  estimation of the robot and object configurations from vision and encoders.
+
+  \link agimus_hpp.planning_request_adapter.PlanningRequestAdapter
+  PlanningRequestAdapter \endlink initializes a client to \c hppcorbaserver
+  and send motion planning requests.
 */
