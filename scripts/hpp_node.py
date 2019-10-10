c638d7aee4907f798dc633be24ec286b40df8d14
Florent Lamiraux <florent@laas.fr>
2019-10-10 09:48:07 +0200

    [Doc] replace \package by \file.

--- scripts/hpp_node.py
+++ scripts/hpp_node.py
@@ -26,7 +26,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-## \package hpp_node.py
+## \file hpp_node.py
 #
 # This file starts a ROS node that retrieves paths planned by hppcorbaserver.
 #
